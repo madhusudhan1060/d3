8360cbd8fcf4a6a1031f551bed2380af6489b1c7
madhusudhan1060 <32870000+madhusudhan1060@users.noreply.github.com>
2022-08-19 18:27:39 +0530

    events

--- builds/d3/js/script.js
+++ builds/d3/js/script.js
@@ -1,29 +1,59 @@
-var bardata = [20, 30, 45, 15];
+// var bardata = [12, 78, 30, 45, 15, 40];
+var bardata = [];
+for (var i = 0; i < 100; i++) {
+    bardata.push(Math.random() * 30);
+}
+
 var height = 400,
     width = 600,
     barWidth = 50,
-    barOffset = 5;
+    barOffset = 5,
+    tempColor;
 
 var yScale = d3.scaleLinear()
     .domain([0, d3.max(bardata)])
     .range([0, height]);
 
+var xScale = d3.scaleBand()
+    .domain(bardata)
+    .paddingInner(.3)
+    .paddingOuter(.1)
+    .range([0, width]);
+
+var colors = d3.scaleLinear()
+    .domain([0, bardata.length * 0.33,
+                bardata.length *.66,
+                bardata.length])
+    .range(['#ffB832', '#C61C6F', '#268BD2', '#85992C']);
+
 d3.select('#viz').append('svg')
   .attr('width', width)
   .attr('height', height)
-  .style('background', '#C9D7D6')
 .selectAll('rect')
     .data(bardata)
     .enter()
     .append('rect')
-    .style('fill', '#C61C6F')
-    .attr('width', barWidth)
+    .attr('fill', function(d, i) {
+        return colors(i);
+    })
+    .attr('width', function(){
+        return xScale.bandwidth();
+    })
     .attr('height', function(d) {
       return yScale(d);
     })
-    .attr('x', function(d, i) {
-      return i*(barWidth + barOffset)
+    .attr('x', function(d) {
+      return xScale(d);
     })
     .attr('y', function(d) {
       return height - yScale(d);
+    })
+    .on('mouseover', function(d) {
+        tempColor = this.style.fill;
+        d3.select(this)
+            .style('fill', 'yellow');
+    })
+    .on('mouseout', function(d) {
+        d3.select(this)
+        .style("fill", tempColor);
     });
