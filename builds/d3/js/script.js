437318c5821e19f6184befc7faf8073f896937c3
madhusudhan1060 <32870000+madhusudhan1060@users.noreply.github.com>
2022-08-20 23:14:01 +0530

    tooltip

--- builds/d3/js/script.js
+++ builds/d3/js/script.js
@@ -1,85 +1,90 @@
-var bardata = [];
-    for (var i = 0; i<50; i++) {
-      bardata.push(Math.random() * 30);
+d3.json('js/data/forecast.json', function(d) {
+    var temperatures = [],
+        height = 400,
+        width = 600;
+
+    var tempColor,
+        yScale,
+        xScale,
+        colors,
+        tooltip,
+        myChart;
+
+    for (var i = 0; i < d.list.length; i++) {
+      temperatures.push(d.list[i].main.temp);
     }
-var height = 400,
-    width = 600,
-    barWidth = 50,
-    barOffset = 5,
-    tempColor;
 
-var yScale = d3.scaleLinear()
-    .domain([0, d3.max(bardata)])
-    .range([0,height]);
+    yScale = d3.scaleLinear()
+        .domain([0, d3.max(temperatures)])
+        .range([0, height]);
 
-var xScale = d3.scaleBand()
-    .domain(bardata)
-    .paddingInner(.3)
-    .paddingOuter(.1)
-    .range([0, width])
+    xScale = d3.scaleBand()
+        .domain(temperatures)
+        .paddingInner(0.1)
+        .paddingOuter(0.1)
+        .range([0, width]);
 
-var colors = d3.scaleLinear()
-    .domain([0, bardata.length *.33,
-                bardata.length *.66,
-                bardata.length
-                ])
-    .range(['#B58929', '#C61C6F',
-            '#268BD2', '#85992C'])
+    colors = d3.scaleLinear()
+        .domain([0, 65, d3.max(temperatures)])
+        .range(["#FFFFFF", "#2D8BCF", "#DA3637"]);
 
-var tooltip = d3.select('body')
-                .append('div')
-                .style('position', 'absolute')
-                .style('padding', '0 10px')
-                .style('background', 'white')
-                .style('opacity', 0);
+    tooltip = d3.select("body")
+        .append("div")
+        .style("position", "absolute")
+        .style("padding", "0 10px")
+        .style("background", "white")
+        .style("opacity", 0);
 
-var myChart = 
-d3.select('#viz').append('svg')
-  .attr('width', width)
-  .attr('height', height)
-.selectAll('rect').data(bardata)
-  .enter().append('rect')
-    .attr('fill', function(d, i) {
-      return colors(i)
-    })
-    .attr('width', function(d) {
-      return xScale.bandwidth();
-    })
-    .attr('height', 0)
-    .attr('x', function(d) {
-      return xScale(d);
-    })
-    .attr('y', height)
-    
-    .on('mouseover', function(d) {
+    var myChart = d3
+      .select("#viz")
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .selectAll("rect")
+      .data(temperatures)
+      .enter()
+      .append("rect")
+      .attr("fill", colors)
+      .attr("width", function (d) {
+        return xScale.bandwidth();
+      })
+      .attr("height", 0)
+      .attr("x", function (d) {
+        return xScale(d);
+      })
+      .attr("y", height)
 
-      tooltip.transition().duration(200)
-        .style('opacity', .9)
+      .on("mouseover", function (d) {
+        tooltip.transition().duration(200).style("opacity", 0.9);
 
-      tooltip.html(d)
-        .style('left', (d3.event.pageX -35) + 'px')
-        .style('top', (d3.event.pageY -30) + 'px')
+        tooltip
+          .html(
+            '<div style="font-size: 14px; font-weight: bold;">' +
+            d + '&deg; </div>'
+          )
+          .style("left", d3.event.pageX - 35 + "px")
+          .style("top", d3.event.pageY - 30 + "px");
 
-      tempColor = this.style.fill;
-      d3.select(this)
-        .style('fill', 'yellow')
-    })
+        tempColor = this.style.fill;
+        d3.select(this).style("fill", "yellow");
+      })
 
-    .on('mouseout', function(d) {
-      d3.select(this)
-        .style('fill', tempColor)
-    });
+      .on("mouseout", function (d) {
+        d3.select(this).style("fill", tempColor);
+      });
 
+    myChart
+      .transition()
+      .attr("height", function (d) {
+        return yScale(d);
+      })
+      .attr("y", function (d) {
+        return height - yScale(d);
+      })
+      .delay(function (d, i) {
+        return i * 20;
+      })
+      .duration(1000)
+      .ease(d3.easeBounceOut);
 
-myChart.transition()
-  .attr('height', function(d) {
-    return yScale(d);
-  })
-  .attr('y', function(d) {
-    return height - yScale(d);
-  })
-  .delay(function(d, i) {
-    return i * 20;
-  })
-  .duration(1000)
-  .ease(d3.easeBounceOut)
+});
